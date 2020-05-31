e056ba9e6d74616b3b2aa763b1a6d7167ec06930
nurzainpradana <pradananurzain@gmail.com>
2020-05-31 13:21:38 +0700

    fetch repo

--- app/src/main/java/com/nurzainpradana/koperasimasjid/fragment/FavoriteDao.java
+++ app/src/main/java/com/nurzainpradana/koperasimasjid/fragment/FavoriteDao.java
@@ -13,16 +13,16 @@ public interface FavoriteDao {
 
     //Add to Favorite
     @Insert
-    public void addData(FavoriteList favoriteList);
+    public void addData(com.nurzainpradana.koperasimasjid.fragment.FavoriteList favoriteList);
 
     @Query("SELECT * FROM favoritelist")
-    public List<FavoriteList> getFavoriteData();
+    public List<com.nurzainpradana.koperasimasjid.fragment.FavoriteList> getFavoriteData();
 
     @Query("SELECT EXISTS (SELECT 1 FROM favoritelist WHERE id=:id)")
     public int isFavorite(int id);
 
     @Delete
-    public void delete(FavoriteList favoriteList);
+    public void delete(com.nurzainpradana.koperasimasjid.fragment.FavoriteList favoriteList);
 
 
 }
