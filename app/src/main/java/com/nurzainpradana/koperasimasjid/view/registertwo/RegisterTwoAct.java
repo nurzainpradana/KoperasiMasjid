1ad5a3124f5336fe363c9a9975a49f41cc3a8b50
nurzainpradana <pradananurzain@gmail.com>
2020-06-07 14:00:24 +0700

    update register two activity

--- app/src/main/java/com/nurzainpradana/koperasimasjid/view/registertwo/RegisterTwoAct.java
+++ app/src/main/java/com/nurzainpradana/koperasimasjid/view/registertwo/RegisterTwoAct.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
